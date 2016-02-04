afbd80e950dc4dec3d11ef37915ce46f675bec0d
steven-pozarycki <spoz@optonline.net>
2016-02-04 17:14:36 -0500

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,45 +1,58 @@
 #!/bin/sh
-#. config.properties
 
-# CREATE STORAGE CONTAINER
-echo curl -i -X PUT \
-  -u ${USER_ID}:${USER_PASSWORD} \
-  https://${ID_DOMAIN}.storage.oraclecloud.com/v1/Storage-$ID_DOMAIN/$APP_NAME
+export ID_DOMAIN=$1
+export USER_ID=$2
+export USER_PASSWORD=$3
+export APP_NAME=$4
+export ARCHIVE_FILE=$5
+export ARCHIVE_LOCAL=target/$ARCHIVE_FILE
+export APAAS_HOST=apaas.us.oraclecloud.com
 
+# CREATE CONTAINER
+echo '\n[info] Creating container\n'
 curl -i -X PUT \
     -u ${USER_ID}:${USER_PASSWORD} \
     https://${ID_DOMAIN}.storage.oraclecloud.com/v1/Storage-$ID_DOMAIN/$APP_NAME
 
 # PUT ARCHIVE IN STORAGE CONTAINER
-echo curl -i -X PUT \
-  -u ${USER_ID}:${USER_PASSWORD} \
-  https://${ID_DOMAIN}.storage.oraclecloud.com/v1/Storage-$ID_DOMAIN/$APP_NAME/$ARCHIVE_FILE \
-      -T $ARCHIVE_LOCAL
+echo '\n[info] Uploading application to storage\n'
 curl -i -X PUT \
   -u ${USER_ID}:${USER_PASSWORD} \
   https://${ID_DOMAIN}.storage.oraclecloud.com/v1/Storage-$ID_DOMAIN/$APP_NAME/$ARCHIVE_FILE \
       -T $ARCHIVE_LOCAL
 
-# CREATE APPLICATION WITH DEPLOYMENT.JSON
-echo curl -i -X POST  \
+# See if application exists
+let httpCode=`curl -i -X GET  \
   -u ${USER_ID}:${USER_PASSWORD} \
   -H "X-ID-TENANT-NAME:${ID_DOMAIN}" \
   -H "Content-Type: multipart/form-data" \
-  -F "name=${APP_NAME}" \
-  -F "runtime=node" \
-  -F "deployment=@deployment-1.json" \
-  -F "subscription=Monthly" \
-  -F archiveURL=${APP_NAME}/${ARCHIVE_FILE} \
-  https://${APAAS_HOST}/paas/service/apaas/api/v1.1/apps/${ID_DOMAIN}
+  -sL -w "%{http_code}" \
+  https://${APAAS_HOST}/paas/service/apaas/api/v1.1/apps/${ID_DOMAIN}/${APP_NAME} \
+  -o /dev/null`
 
-curl -i -X POST  \
-  -u ${USER_ID}:${USER_PASSWORD} \
-  -H "X-ID-TENANT-NAME:${ID_DOMAIN}" \
-  -H "Content-Type: multipart/form-data" \
-  -F "name=${APP_NAME}" \
-  -F "runtime=node" \
-  -F "deployment=@deployment-1.json" \
-  -F "subscription=Monthly" \
-  -F archiveURL=${APP_NAME}/${ARCHIVE_FILE} \
-  https://${APAAS_HOST}/paas/service/apaas/api/v1.1/apps/${ID_DOMAIN}
+# If application exists...
+if [ $httpCode == 200 ]
+then
+  # Update application
+  echo '\n[info] Updating application...\n'
+  curl -i -X PUT  \
+    -u ${USER_ID}:${USER_PASSWORD} \
+    -H "X-ID-TENANT-NAME:${ID_DOMAIN}" \
+    -H "Content-Type: multipart/form-data" \
+    -F archiveURL=${APP_NAME}/${ARCHIVE_FILE} \
+    https://${APAAS_HOST}/paas/service/apaas/api/v1.1/apps/${ID_DOMAIN}/${APP_NAME}
+else
+  # Create application and deploy
+  echo '\n[info] Creating application...\n'
+  curl -i -X POST  \
+    -u ${USER_ID}:${USER_PASSWORD} \
+    -H "X-ID-TENANT-NAME:${ID_DOMAIN}" \
+    -H "Content-Type: multipart/form-data" \
+    -F "name=${APP_NAME}" \
+    -F "runtime=java" \
+    -F "subscription=Hourly" \
+    -F archiveURL=${APP_NAME}/${ARCHIVE_FILE} \
+    https://${APAAS_HOST}/paas/service/apaas/api/v1.1/apps/${ID_DOMAIN}
+fi
+echo '\n[info] Deployment complete\n'
 
